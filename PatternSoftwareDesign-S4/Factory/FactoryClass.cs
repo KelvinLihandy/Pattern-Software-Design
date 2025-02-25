73cb4655d1bc330522286767fc376fa7f1e8b7c6
KelvinLihandy <kelvinlihandy2005@gmail.com>
2025-02-25 19:06:46 +0700

    update session 4

---
+++ PatternSoftwareDesign-S4/Factory/FactoryClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PatternSoftwareDesign_S4.Model;
+
+namespace PatternSoftwareDesign_S4.Factory
+{
+    public class FactoryClass
+    {
+        public static cloth createCloth(String id, String brand, String size, int stock, int price, String clothTypeId)
+        {
+            cloth cloth = new cloth();
+            cloth.id = id;
+            cloth.brand = brand;
+            cloth.size = size;
+            cloth.stock = stock;
+            cloth.price = price;
+            cloth.cloth_type_id = clothTypeId;
+            return cloth;
+        }
+        public static cloth createCloth(String id, String brand, String size, int price, String clothTypeId)
+        {
+            cloth cloth = new cloth();
+            cloth.id = id;
+            cloth.brand = brand;
+            cloth.size = size;
+            cloth.stock = 0;
+            cloth.price = price;
+            cloth.cloth_type_id = clothTypeId;
+            return cloth;
+        }
+    }
+}
---
+++ PatternSoftwareDesign-S4/Main/Main.aspx.cs
@@ -0,0 +1,32 @@
+﻿using PatternSoftwareDesign_S4.Factory;
+using PatternSoftwareDesign_S4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PatternSoftwareDesign_S4.Main
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        Database1Entities db = new Database1Entities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var clothesList = new List<cloth>()
+            {
+                FactoryClass.createCloth("CU001", "SKIBIDI", "M", 2, 15000, "CT001"),
+                FactoryClass.createCloth("CU002", "RIZZ", "L", 23000, "CT002")
+            };
+            foreach (cloth cloth in clothesList)
+            {
+                clothLabel.Text += $"Cloth ID: {cloth.id}, " +
+                    $"Cloth Brand: {cloth.brand}, " +
+                    $"Cloth Size: {cloth.size}, " +
+                    $"Cloth Stock: {cloth.stock}, " +
+                    $"Cloth Type ID: {cloth.cloth_type_id} ";
+            }
+        }
+    }
+}
